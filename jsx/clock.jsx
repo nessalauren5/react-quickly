26adcb51e75153f455759dcf22b911dae946e465
ness <nessa.lauren5@gmail.com>
2019-02-27 21:09:23 -0800

    stateful react element via jsx

--- jsx/clock.jsx
+++ jsx/clock.jsx
@@ -1,14 +1,26 @@
 class Clock extends React.Component{
 	constructor(props){
 		super(props)
+		this.launchClock()
 		this.state={currentTime:(new Date()).toLocaleString()}
+		
 	}
+	/* method to update clock*/
+launchClock(){
+	setInterval(() => {
+		console.log('updating the time.')
+		this.setState({ 
+			currentTime : (new Date()).toLocaleString()
+		})
+	},1000)
+}
 	render(){
+	    console.log('rendering clock')
 		return <div>{this.state.currentTime}</div>
 	}
 }
 
 ReactDOM.render(
-	<Clock/>
+	<Clock />,
 	document.getElementById('clock-content')
 )
