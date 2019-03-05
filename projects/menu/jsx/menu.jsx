d4049ebe0d546f4892479120816a2d6a22f44c62
ness <nessa.lauren5@gmail.com>
2019-03-05 11:32:06 -0800

    use fetch to load menu items from external source

--- projects/menu/jsx/menu.jsx
+++ projects/menu/jsx/menu.jsx
@@ -1,22 +1,41 @@
 const menuOptions = ['Home', 'Menu', 'About', 'Services', 'Portfolio', 'Contact Us']
 class Menu extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {data_items:menuOptions}
+    }
+
+    componentDidMount() {
+        fetch(this.props.url)
+            .then(response => response.json())
+            .then(data => this.setState({data_items:data.items}))
+            .catch((error) => {
+                console.log('unable to retrieve data via fetch api')
+                console.log(error.message)
+            })
+    }
+
+    componentWillUpdate(){
+        console.log("Received new props, re-rendering")
+    }
+
     render(){
-        let menus = ['Home',
-            'Menu',
-            'About',
-            'Services',
-            'Portfolio',
-            'Contact Us']//this.props.menus
+        let menus = this.state.data_items
+
         return <div>
-            {menus.map((v,i) => {
+            {
+                menus.map((v,i) => {
                 return React.createElement('div',
                     {key: i},
                     <Link label={v}/>)
-            })}
+            }
+            )
+            }
         </div>
 
     }
 }
+
 class Link extends React.Component{
     render(){
         const url = '/'
@@ -38,6 +57,6 @@ class Link extends React.Component{
 
 
 ReactDOM.render(
-    <Menu/>,
+    <Menu url="menu-items.json"/>,
     document.getElementById('menu')
 )
