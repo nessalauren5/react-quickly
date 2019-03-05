8ad97669e10d0952f0f4d01bc3c8242015434b1c
ness <nessa.lauren5@gmail.com>
2019-03-04 19:08:32 -0800

    no message

--- jsx/first.jsx
+++ jsx/first.jsx
@@ -1,10 +1,11 @@
 class LinkItem extends React.Component{
 	render(){
+		//jsx
 		return <a href={this.props.url}  title={this.props.title}>{this.props.label}</a>
 	}
 }
 
 ReactDOM.render(
-	React.createElement(LinkItem,{url:"https://www.google.com",title:"Take Me to Google",label:"Click Me!"}),
+	React.createElement(LinkItem,{url:"https://www.google.com",title:"Take Me to Google",label:"Click Me!"}),//standard js
 	document.getElementById('url-content')
 )
